a07c561f7192bfa6ffcabc2c7c87623972f36e7e
Yuliia Yefimenko <yuliia.yefimenko@privatbank.ua>
2021-10-16 03:09:52 +0300

    init commit

--- src/com/pb/yefimenko/hw2/Calculator.java
+++ src/com/pb/yefimenko/hw2/Calculator.java
@@ -5,40 +5,35 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int operand1;
-        int operand2;
-        String symbol;
-        String sign = "+, -, *, /";
         System.out.println("Введите целое цифровое значение operand1 ");
-        operand1 = scan.nextInt();
+        int operand1 = scan.nextInt();
         System.out.println("Введите целое цифровое значение operand2 ");
-        operand2 = scan.nextInt();
-        System.out.println("+, -, *, /");
-        scan.nextLine();
-        symbol = scan.nextLine();
-
-        System.out.println(" operand1 =" + operand1+ " operand2 = " + operand2 + "symbol = " + symbol );}
-
-
-    switch ( symbol ){
-        case "+":
-            System.out.println("operand1+operand2");
-            break;
-        case "-":
-            System.out.println("operand1-operand2");
-            break;
-        case "*":
-            System.out.println("operand1*operand2");
-            break;
-        case "/":
-            if ( operand2 < 0) {
-                System.out.print("Ошибка! Деление на ноль невозможно, ЕСЛИ ВЫ НЕ КЕРНЕС");
-            } else {
-                System.out.print("operand1/operand2");
-            }
-            break;
-        default:
-            System.out.println("Неверная операция, введите допустимое арифметическое действие");
+        int operand2 = scan.nextInt();
+        System.out.println("Введите допустимый знак арифметической операции + - * /");
+        ????????
+        String symbol = scan.nextInt();;  или след. назначение символа и вместо symbol правильно написать sign
+        int symbol = scan.nextInt();
+        switch (symbol) {
+            case "+":
+                System.out.println("operand1+operand2");
+                break;
+            case "-":
+                System.out.println("operand1-operand2");
+                break;
+            case "*":
+                System.out.println("operand1*operand2");
+                break;
+            case "/":
+                if (operand2 < 0) {
+                    System.out.print("Ошибка! Деление на ноль невозможно, ЕСЛИ ВЫ НЕ КЕРНЕС");
+                } else {
+                    System.out.print("operand1/operand2");
+                }
+                break;
+            default:
+                System.out.println("Неверная операция, введите допустимое арифметическое действие");
+        }
+        System.out.println("\nРезультат: \n");
+        System.out.println(" operand1 =" + operand1 + " operand2 = " + operand2 + "symbol = " + symbol);
     }
-
 }
