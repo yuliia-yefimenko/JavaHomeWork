cc1fd3806eb3b4c1017a725374dfc0fdd5f36bc6
Yuliia Yefimenko <yuliia.yefimenko@privatbank.ua>
2021-10-25 03:40:21 +0300

    init commit

--- src/com/pb/yefimenko/hw3/BingoDoWhile.java
+++ src/com/pb/yefimenko/hw3/BingoDoWhile.java
@@ -2,7 +2,7 @@ package com.pb.yefimenko.hw3;
 
 import java.util.Scanner;
 
-public class Bingo {
+public class BingoDoWhile {
     public static void main (String[] args){
         System.out.println("Отгадайте целое число в диапазоне от 0 до 100");
         System.out.println("Для выхода из программы введите - exit.");
@@ -10,25 +10,23 @@ public class Bingo {
         int attempt = 0;
         int counter = 25;
         Scanner in = new Scanner(System.in);
-        while (attempt<MAX_ATTEMPT){
-            attempt++;
-            System.out.println("Попытка"+attempt+":");
-            counter = in.nextInt();
+        counter = in.nextInt();
+        do {attempt++;
+            System.out.println("Попытка" + attempt + ":");
             if (counter.equals("exit")) {
                 break;
             }
-            if (counter<0 | counter>100) {
+            if (counter < 0 | counter > 100) {
                 System.out.println("Число не входит в диапазон");
                 break;
             }
-            if (counter>0 | counter<24 & counter>26) {
+            if (counter > 0 | counter < 24 & counter > 26) {
                 System.out.println("Не угадали");
                 continue;
             }
-            if (counter==25){
-                System.out.println("Урра!Вы угадали с"+attempt+"попытки");
-                break;
-            }
+        } while((attempt<MAX_ATTEMPT) || counter==25);{
+            System.out.println("Урра!Вы угадали с"+attempt+"попытки");
+            break;)
         }
         System.out.println("Конец игры");
     }
