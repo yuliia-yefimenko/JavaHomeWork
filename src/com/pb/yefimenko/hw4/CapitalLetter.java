3c1a4c67b8b31c08e8d473218ba9142f82c0fa8d
Yuliia Yefimenko <yuliia.yefimenko@privatbank.ua>
2021-10-26 21:18:01 +0300

    init commit

---
+++ src/com/pb/yefimenko/hw4/CapitalLetter.java
@@ -0,0 +1,17 @@
+package com.pb.yefimenko.hw4;
+
+import java.util.Scanner;
+
+public class CapitalLetter {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String i =
+
+        int [] array = new int[10];
+        int i = array.length;
+        array [0]=scan.nextInt();
+
+        System.out.println("Введите предложение");
+
+    }
+}
