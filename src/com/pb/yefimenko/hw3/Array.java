3c5042a2cdef46757e37feaae1dfca56fbc3b937
Yuliia Yefimenko <yuliia.yefimenko@privatbank.ua>
2021-10-26 04:48:39 +0300

    init commit

--- src/com/pb/yefimenko/hw3/Array.java
+++ src/com/pb/yefimenko/hw3/Array.java
@@ -7,7 +7,6 @@ public class Array {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         int [] array = new int[10];
-
         int i = array.length;
         int sum = 0;
         array [0]=scan.nextInt();
@@ -29,10 +28,11 @@ public class Array {
         array [8]=scan.nextInt();
         System.out.println(array[8]);
         array [9]=scan.nextInt();
-        System.out.println(array[9]);    // упрощаем ТАК МОЖНО? Ведь я хочу чтоб просуммировалось, то , что ввел пользователь
-        System.out.println(Arrays.toString(array));
-
-        for ( int i = 0; i < array.length){   // сумма массива
+        System.out.println(array[9]);    // упрощаем ТАК МОЖНО? Ведь я хочу чтоб вывелись данные, что ввел пользователь
+        for( int i = 0; i < array.length; i++) {  // или вывод на экран циклом
+            System.out.println(array[i]);
+        }
+        for ( int i = 0; i < array.length){   // сумма всего массива  А КАК МОЖНО УПРОСТИТЬ?
             sum = (array [1] + array [2] + array [3] + array [4] + array [5] + array [6] + array [7] + array [8]  + array [9] );
             System.out.println("Сумма" +sum );
         }
