a8e42367e6b87c5b8a4551c75ddfe70c417a129d
Yuliia Yefimenko <yuliia.yefimenko@privatbank.ua>
2021-10-26 03:53:39 +0300

    init commit

--- src/com/pb/yefimenko/hw3/Bingo.java
+++ src/com/pb/yefimenko/hw3/Bingo.java
@@ -1,31 +1,29 @@
 package com.pb.yefimenko.hw3;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Bingo {
     public static void main (String[] args){
         System.out.println("Отгадайте целое число в диапазоне от 0 до 100");
-        System.out.println("Для выхода из программы введите - exit.");
+        System.out.println("Для выхода из программы введите число меньше 0");
+        Random random = new Random();
+        int counter = (int) ( Math.random() * 101 );
         final int MAX_ATTEMPT = 101; // ведь вариантов от 0 до 100
         int attempt = 0;
-        int counter = 25;
         Scanner in = new Scanner(System.in);
         while (attempt<MAX_ATTEMPT){
             attempt++;
             System.out.println("Попытка"+attempt+":");
             counter = in.nextInt();
-            if (counter==("exit")) {
+            if (counter<0) {
                 break;
             }
-            if (counter<0 | counter>100) {
+            if (counter < 0 | counter>100) {
                 System.out.println("Число не входит в диапазон");
                 break;
             }
-            if (counter>0 | counter<24 & counter>26) {
-                System.out.println("Не угадали");
-                continue;
-            }
-            if (counter==25){
+            if (counter == int counter){
                 System.out.println("Урра!Вы угадали с"+attempt+"попытки");
                 break;
             }
