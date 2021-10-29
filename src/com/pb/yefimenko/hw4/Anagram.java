bc3802e11684dff0fa24f136493c60e0740f89fb
Yuliia Yefimenko <yuliia.yefimenko@privatbank.ua>
2021-10-29 07:11:19 +0300

    init commit

--- src/com/pb/yefimenko/hw4/Anagram.java
+++ src/com/pb/yefimenko/hw4/Anagram.java
@@ -3,15 +3,26 @@ package com.pb.yefimenko.hw4;
 import java.util.Scanner;
 
 public class Anagram {
-    public static String format(String, Object... args)
-    String cleanLine = string.replaceAll("[\\W]", "");
-    Character.isLetterOrDigit
-            st = st.replaceAll("\\s","")
-}
+    public static String format(String[] args){
+        stringA = stringA.replaceAll("[^\\da-zA-Zа-яёА-ЯЁ]", "");
+    }
+
+    //Character.isLetterOrDigit
     public static void main(String[] args){
-    Scanner scan = new Scanner(System.in);
-    System.out.println("Введите первую строку");
-    String 1= scan.nextLine();
-    System.out.println("Введите вторую строку");
-    String 2= scan.nextLine();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите первую строку");
+        String s1 = scan.nextLine();
+        System.out.println("Введите вторую строку");
+        String s2= scan.nextLine();
+        String format(String[] args);
+        string s1 = string s1.replaceAll("[^\\da-zA-Zа-яёА-ЯЁ]", "");
+        strings2 = strings2.replaceAll("[^\\da-zA-Zа-яёА-ЯЁ]", "");
+        String s1 = s1.length();
+        String s2 = s2.length();
+        if ( s1.length() != s2.length()) {
+            return false;}
+        else if (s1.length() == s2.length()){
+            return true
+                    System.out.println("Слова анаграммы");
+        }
 }
