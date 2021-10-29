ce54dea55b3a1d7b5bdedb91e1ffa937c97274f0
Yuliia Yefimenko <yuliia.yefimenko@privatbank.ua>
2021-10-29 05:25:48 +0300

    init commit

--- src/com/pb/yefimenko/hw4/CapitalLetter.java
+++ src/com/pb/yefimenko/hw4/CapitalLetter.java
@@ -3,15 +3,18 @@ package com.pb.yefimenko.hw4;
 import java.util.Scanner;
 
 public class CapitalLetter {
+    static void print (String in){              // метод напечатать строку
+        System.out.println(in);
+    }
+    static toUpperCase(char[] charArr){
+    }
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        String i =
-
-        int [] array = new int[10];
-        int i = array.length;
-        array [0]=scan.nextInt();
-
-        System.out.println("Введите предложение");
-
+        System.out.println("Введите строку");   // пользователь ввел строку
+        String in = scan.nextLine();
+        print (String in);
+        char[] charArr = in.toCharArray();      // преобраз. строку в массив
+        System.out.println( in.toString(charArr));
+        System.out.println(in.toUpperCase());   // все символы в верхний регистр
     }
 }
