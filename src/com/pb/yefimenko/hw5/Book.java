b8c413f916f18dc1a7e0718ca851948cf8cacd32
Yuliia Yefimenko <yuliia.yefimenko@privatbank.ua>
2021-11-07 21:03:04 +0200

    init commit

--- src/com/pb/yefimenko/hw5/Book.java
+++ src/com/pb/yefimenko/hw5/Book.java
@@ -4,4 +4,14 @@ public class Book {
     String nazva;
     String avtor;
     int year;
+    int bookCount=0;
+    public static int takeBook() {
+        return int bookCount++;
+    }
+    public Book(String nazva,String avtor,int year,int bookCount) {
+        this.nazva = nazva;
+        this.avtor = avtor;
+        this.year =  year;
+        bookCount ++;
+    }
 }
--- src/com/pb/yefimenko/hw5/Reader.java
+++ src/com/pb/yefimenko/hw5/Reader.java
@@ -6,34 +6,41 @@ public class Reader {
     String fakultet;
     String dateOfBirth;
     int telephone;
-    Object Reader;
-    public static void main(String[] args) {
-        Book book1 = new Book(nazva:"Приключение", avtor:"Автор1 А.А.", year: 2000 );
-        Book book2 = new Book(nazva: "Словарь", avtor:"Автор2 Б.Б.", year: 2010);
-        Book book3 = new Book(nazva: "Энциклопедия",avtor:"Автор3 В.В.", year: 2020 );
-        Book[] ArrayBook = {book1, book2, book3};
 
-        for (Book x = ArrayBook) {
+    public Reader (String name, int biletNumber, String fakultet,  String dateOfBirth, int telephone) {
+        this.name = name;
+        this.biletNumber = biletNumber;
+        this.fakultet = fakultet;
+        this.dateOfBirth = dateOfBirth;
+        this.telephone = telephone;
+    }
+    Book book1 = new Book ("Приключения","Иванов А.А.",2000);
+    Book book2 = new Book ("Словарь","Сидоров Б.Б.",2010);
+    Book book3 = new Book ("Энциклопедия","Петров В.В.",2020);
+    Book[] ArrayBook = {book1, book2, book3};
+    for (Book x = ArrayBook) {
             System.out.println(x.nazva+x.avtor+x.year());
         }
-        Reader r1= new Reader( name:"Иванов И.И.",biletNumber:1111, fakultet:"инженерный",
-                dateOfBirth: "10.10.2000", telephone:0991234567 );
-        Reader r2= new Reader( name:"Петров П.П.",biletNumber:2222, fakultet:"экономический",
-                dateOfBirth: "10.10.2010", telephone:0501234567 );
-        Reader r3= new Reader( name:"Васечкин В.В.",biletNumber:3333, fakultet:"гуманитарный",
-                dateOfBirth: "10.10.2020", telephone:0631234567 );
-        Reader[] R ={ r1,r2,r3 };
+    Reader r1= new Reader( "Иванов И.И.",1111, "инженерный",
+                "10.10.2000", 0991234567 );
+    Reader r2= new Reader( "Петров П.П.",2222, "экономический",
+                "10.10.2010",0501234567 );
+    Reader r3= new Reader( "Васечкин В.В.",3333, "гуманитарный",
+                "10.10.2020", 0631234567 );
+    Reader[] R ={ r1,r2,r3 };
         for (Reader y = R) {
-            System.out.println(y.name+y.biletNumber+y.fakultet+y.dateOfBirth+y.telephone());
-
-
-        String takeBook(){
-            for (x=book1;x<ArrayBook.length;x++),(y=r1,y<R.length; y++){
-            return System.out.println("[ Читатель"+y+"взял книгу"+ x+"название:"+x.nazva+", автор: "+x.avtor+
-                    ", год: "+x.year+"]");}
-        }
-        String returnBook(){
-            return "[ Вернул книгу название: "+nazva+", автор: "+avtor+", год: "+year+"]";
+            System.out.println(y.name+y.biletNumber+y.fakultet+y.dateOfBirth+y.telephone());}
+    public void takeBook (int bookCount) {
+        for (x=book1;x<ArrayBook.length;x++),(y=r1,y<R.length; y++){
+            return System.out.println("[ Читатель"+y+"взял книгу"+ x+"название:"+x.nazva+", автор: "
+                    +x.avtor+ ", год: "+x.year+"]")}
+        System.out.println(this.name + " взял " + bookCount + " книг");
+    }
+    public void returnBook(int bookCount){
+        for (x=book1;x<ArrayBook.length;x++),(y=r1,y<R.length; y++){
+            return System.out.println("[ Читатель"+y+"взял книгу"+ x+"название:"+x.nazva+", автор: "
+                    +x.avtor+ ", год: "+x.year+"]");}
+        System.out.println(this.name + " вернул " + bookCount + " книг");
         }
     }
 
@@ -43,12 +50,7 @@ public class Reader {
 
 
     ///// public void takeBook (String name, String... namebooks) {
-    //        System.out.println(name + "взял несколько книг" + namebook);
-    //        for (String namebook : namebooks) {
-    //
-    //            System.out.println(namebook);
-    //        создавать массив примерно так:
-    //Reader[] readerArray = new Reader[4];
+
     //
     //получать свойства и методы так:
     //bookArray[1].getTitle()
